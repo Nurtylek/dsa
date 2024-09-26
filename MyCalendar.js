49766450f27056ba7ac95db4a472255969f13756
Nurtilek Koshkar <38660474+Nurtylek@users.noreply.github.com>
2024-09-26 09:20:14 +0500

    Create MyCalendar.js

---
+++ MyCalendar.js
@@ -0,0 +1,25 @@
+var MyCalendar = function() {
+    this.events = [];
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(start, end) {
+    for (const [s, e] of this.events) {
+        if (e > start && end > s) {
+            return false
+        }
+    }
+
+    this.events.push([start, end]);
+    return true
+};
+
+/** 
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
